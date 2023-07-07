6838c3a9b109ea4c11fe627ceb3335d371ebd1d0
chang <kennethchangla@gmail.com>
2023-07-07 11:24:35 +0800

    [feat] rotate donut

--- src/mockServer.py
+++ src/mockServer.py
@@ -1,4 +1,4 @@
-from util import color_conversion, coordinate_to_distance
+from util import color_conversion, coordinate_to_distance, three_to_two
 from request import http_request, point_cloud
 import time
 import numpy as np
@@ -8,19 +8,28 @@ import numpy as np
 def process_data_periodically():
     while True:
         # Retrieve point cloud data
-        point_cloud_data = point_cloud.get_point_cloud_data()
+        # point_cloud_data = point_cloud.get_point_cloud_data()
 
         # Extract distances from point_cloud data
-        distances = coordinate_to_distance.coordinate2distance(point_cloud_data)
-
+        # distances = coordinate_to_distance.coordinate2distance(point_cloud_data)
+        three_to_two.A += three_to_two.theta_spacing
+        three_to_two.B += three_to_two.phi_spacing
+        mock_coordinate = three_to_two.donut(three_to_two.A,three_to_two.B)
+        mock_distances = coordinate_to_distance.coordinate2distance(mock_coordinate)
         # Call the distance2color function
-        colors = color_conversion.distance2color(distances)
+        # colors = color_conversion.distance2color(distances)
+        mock_colors = color_conversion.distance2color(mock_distances)
 
+        zipped_data = []
+
+        for coord, color in zip(mock_coordinate, mock_colors):
+            coord['color'] = color
+            zipped_data.append(coord)
+        
         # Send the color request
-        http_request.send_color_request(colors)
-
+        http_request.send_color_request(zipped_data)
         # Pause for 1 second
-        time.sleep(1)
+        time.sleep(0.005)
 
 # Start the data processing
 process_data_periodically()
---
+++ src/util/three_to_two.py
@@ -0,0 +1,61 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+screen_size = 40
+theta_spacing = 0.14
+phi_spacing = 0.04
+
+A = 1
+B = 1
+R1 = 1
+R2 = 2
+K2 = 5
+K1 = screen_size * K2 * 3 / (8 * (R1 + R2))
+def donut(A: float, B: float) -> np.ndarray:
+    """
+    Returns a frame of the spinning 3D donut.
+    Based on the pseudocode from: https://www.a1k0n.net/2011/07/20/donut-math.html
+    """
+    cos_A = np.cos(A)
+    sin_A = np.sin(A)
+    cos_B = np.cos(B)
+    sin_B = np.sin(B)
+
+    output = np.full((screen_size, screen_size), " ")  # (40, 40)
+    zbuffer = np.zeros((screen_size, screen_size))  # (40, 40)
+
+    cos_phi = np.cos(phi := np.arange(0, 2 * np.pi, phi_spacing))  # (315,)
+    sin_phi = np.sin(phi)  # (315,)
+    cos_theta = np.cos(theta := np.arange(0, 2 * np.pi, theta_spacing))  # (90,)
+    sin_theta = np.sin(theta)  # (90,)
+    circle_x = R2 + R1 * cos_theta  # (90,)
+    circle_y = R1 * sin_theta  # (90,)
+
+    x = (np.outer(cos_B * cos_phi + sin_A * sin_B * sin_phi, circle_x) - circle_y * cos_A * sin_B).T  # (90, 315)
+    y = (np.outer(sin_B * cos_phi - sin_A * cos_B * sin_phi, circle_x) + circle_y * cos_A * cos_B).T  # (90, 315)
+    z = ((K2 + cos_A * np.outer(sin_phi, circle_x)) + circle_y * sin_A).T  # (90, 315)
+    ooz = np.reciprocal(z)  # Calculates 1/z
+    xp = (screen_size / 2 + K1 * ooz * x).astype(int)  # (90, 315)
+    yp = (screen_size / 2 - K1 * ooz * y).astype(int)  # (90, 315)
+    L1 = (((np.outer(cos_phi, cos_theta) * sin_B) - cos_A * np.outer(sin_phi, cos_theta)) - sin_A * sin_theta)  # (315, 90)
+    L2 = cos_B * (cos_A * sin_theta - np.outer(sin_phi, cos_theta * sin_A))  # (315, 90)
+    L = np.around(((L1 + L2) * 8)).astype(int).T  # (90, 315)
+    result = []
+    for i in range(len(x)):
+        for j in range(len(x[i])):
+            point = {
+                "x": x[i][j],
+                "y": y[i][j],
+                "z": z[i][j],
+            }
+            result.append(point)
+    return np.array(result)
+
+# Create a 2-dimensional array with Pascal's Triangle values
+# pascal_array = pascals_triangle(100)
+
+def distance2color(distances, max_dist=15.0):
+    cmap = plt.cm.jet_r
+    i = distances / max_dist
+    i = cmap(i.flatten())[:, :3] * 255
+    return i.reshape(distances.shape[0], distances.shape[1], 3).tolist()
